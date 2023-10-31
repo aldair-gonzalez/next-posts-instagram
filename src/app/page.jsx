eafa2238d9ae7113ce8c4255c3646aecdcb17a5e
Aldair González <aldairgome97@gmail.com>
2023-10-30 19:30:32 -0600

    Mejorando la app

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const getFeed = async () => {
 	const { INSTAGRAM_KEY } = process.env;
-	const url = `https://graph.instagram.com/me/media?fields=id,username,caption,media_url,timestamp,media_type,permalink&access_token=${INSTAGRAM_KEY}`;
+	const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${INSTAGRAM_KEY}`;
 	const data = await fetch(url);
 	const res = data.json();
 	return res;
@@ -20,14 +20,17 @@ export default async function Page({ children }) {
 					<h1 className="mt-5 text-center uppercase font-black text-2xl">
             Instagram posts
 					</h1>
-					<div className="w-full mt-10">
+					<p className='text-center mt-5'>
+            Esta app obtiene las útimas publicaciones del perfil de un usuario de Instagram y las muestra en la siguiente galería
+					</p>
+					<div className="w-full mt-10 grid grid-cols-2 gap-4">
 						{
 							images && images.map((image) => (
-								<Link key={image.id} href={image.permalink} target='_blank'>
-									<img
-										src={image.media_url}
-										alt={image.media_type}
-									/>
+								<Link className='Image-Link overflow-hidden w-full h-full relative' key={image.id} href={image.permalink} target='_blank'>
+									<img className='w-full h-full' src={image.media_url} alt={image.media_type} />
+									<div className="Image-Overlay pointer-events-none bg-black bg-opacity-50 text-white font-semibold uppercase text-lg absolute w-full h-full top-0 left-0 flex items-center justify-center text-center">
+                    Ver publicación
+									</div>
 								</Link>
 							))
 						}

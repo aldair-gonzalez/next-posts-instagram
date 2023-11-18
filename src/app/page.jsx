28cf9bc5f29ebfc65f2cb77377870875e29fea38
Aldair González <aldairgome97@gmail.com>
2023-11-18 12:15:26 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,12 @@ const getFeed = async () => {
 	const { INSTAGRAM_KEY } = process.env;
 	const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink,thumbnail&access_token=${INSTAGRAM_KEY}`;
 	const res = await fetch(url);
-	const data = res.json();
-	return data;
+	const data = await res.json();
+	return await data.data;
 };
 
 export default async function Page({ children }) {
 	const feed = await getFeed();
-	const images = await feed.data;
 
 	return (
 		<main className="w-screen min-h-screen overflow-x-hidden">
@@ -25,7 +24,7 @@ export default async function Page({ children }) {
 					</p>
 					<div className="w-full mt-10 grid grid-cols-2 gap-4">
 						{
-							images && images.map((image) => (
+							feed && feed.map((image) => (
 								<Link className='Image-Link overflow-hidden w-full h-full relative' key={image.id} href={image.permalink} target='_blank'>
 									<Image className='w-full h-full' src={image.media_url} alt={image.caption} width={1000} height={1000} />
 									<div className="Image-Overlay pointer-events-none bg-black bg-opacity-50 text-white font-semibold uppercase text-lg absolute w-full h-full top-0 left-0 flex items-center justify-center text-center">
